ad5fb545601ccf1825da0a15f2472cdefe844416
Shlw <shlw_kevin@hotmail.com>
2017-05-30 23:07:39 +0800

    add pre eval

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -11,86 +11,49 @@
 using namespace std;
 extern int bitcount[1 << 12];
 
-/*
-double evaluate2(Board a, const Block &block, double &inh)
-{
-    a.place(block);
-    int rowelim = a.eliminate().first;
-
-    double land = block.x - rowelim - blockHalfHeight[block.t][block.o]
-                  + (blockHeight[block.t][block.o] - 1) / 2.0 - 1;
-
-    int cntdown[MAPHEIGHT + 2][MAPWIDTH + 2] = {};
-    for (int i = 1; i <= MAPHEIGHT; ++i)
-        for (int j = 1; j <= MAPWIDTH; ++j)
-            if (!a[i][j])
-                cntdown[i][j] = 1 + cntdown[i - 1][j];
-
-
-    bool visible[MAPHEIGHT+2][MAPWIDTH+2]={};
-    for (int i=1;i<=MAPWIDTH;++i) visible[MAPHEIGHT+1][i]=1;
-    for (int i=MAPHEIGHT;i>0;--i)
-        for (int j=1;j<=MAPWIDTH;++j)
-            visible[i][j]=visible[i+1][j]&&(!a[i][j]);
-
-
-    int rowtrans = 0;
-    for (int i = 1; i <= MAPHEIGHT; ++i)
-        for (int j = 1; j <= MAPWIDTH + 1; ++j)
-            if (!!a[i][j] != !!a[i][j - 1])
-                ++rowtrans;
-
-    int holenum = 0;
-    int row[MAPWIDTH + 2] = {};
-    for (int i = MAPHEIGHT - 1; i > 0; --i) {
-        for (int j = 1; j <= MAPWIDTH; ++j)
-            row[j] = (!a[i][j]) & (!!a[i + 1][j] | row[j]);
-        for (int j = 1; j <= MAPWIDTH; ++j)
-            holenum += row[j];
-    }
-
-    int coltrans = 0;
-    for (int i = 1; i <= MAPHEIGHT; ++i)
-        for (int j = 1; j <= MAPWIDTH; ++j)
-            if (!!a[i][j] != !!a[i - 1][j])
-                ++coltrans;
-
-    int wellsum = 0;
-    for (int i = 1; i <= MAPHEIGHT; ++i)
-        for (int j = 1; j <= MAPWIDTH; ++j)
-            if (!a[i][j] && a[i][j - 1] && a[i][j + 1])
-                wellsum += cntdown[i][j];
-
-    int maxheight = 0;
-    for (int i = MAPHEIGHT; i >= 1; --i) {
-        int cnt = 0;
-        for (int j = 1; j <= MAPWIDTH; ++j)
-            if (a[i][j])
-                ++cnt;
-        if (cnt) {
-            maxheight = i;
-            break;
-        }
-    }
-
-    inh = - 4.500158825082766 * land
-            + 3.4181268101392694 * rowelim;
-    return  - 4.500158825082766 * land
-            + 3.4181268101392694 * rowelim
-            - 3.2178882868487753 * rowtrans
-            - 9.348695305445199 * coltrans
-            - 7.899265427351652 * holenum
-            - 3.3855972247263626 * wellsum;
-}
-*/
-double evaluate2_sweet(Board brd, const Block &block, double &inh)
-{
+void eval(Board& brd, const Block& block, int& ele, int& ht) {
     brd.place(block);
     int nobase;
     pair<int, int> elim;
     brd.eliminate(&block, &elim, &nobase);
-    int *rows = brd.rows;
-    int *cols = brd.cols;
+    ele = elim.first;
+    for (int i = MAPHEIGHT; i > 0; ++i)
+        if (brd.rows[i]) {
+            ht = i;
+            break;
+        }
+}
+
+void precalc(GameBoard& gameBoard, int& myele, int& enemyele, int& enemyht) {
+    std::vector<Tetris> myloc;
+    gameBoard.getPlaces(gameBoard.currBotColor, gameBoard.currBlockType, myloc);
+    std::vector<Tetris> enemyloc;
+    gameBoard.getPlaces(gameBoard.enemyColor, gameBoard.enemyType, enemyloc);
+    myele = 0;
+    enemyele = 0;
+    enemyht = 20;
+    for (auto& loc : myloc) {
+        int ht, ele;
+        Board brd = Board(gameBoard.currBotColor, gameBoard);
+        eval(brd, Block(loc), ele, ht);
+        myele = std::max(myele, ele);
+    }
+    for (auto& loc : enemyloc) {
+        int ht, ele;
+        Board brd = Board(gameBoard.enemyColor, gameBoard);
+        eval(brd, Block(loc), ele, ht);
+        enemyele = std::max(enemyele, ele);
+        enemyht = std::min(enemyht, ht);
+    }
+}
+
+double evaluate2_sweet(Board brd, const Block& block, double& inh) {
+    brd.place(block);
+    int nobase;
+    pair<int, int> elim;
+    brd.eliminate(&block, &elim, &nobase);
+    int* rows = brd.rows;
+    int* cols = brd.cols;
 
     double sweet = 0;
     if (nobase == 0) {
